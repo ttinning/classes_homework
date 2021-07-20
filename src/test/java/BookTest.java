90e51a1554d4e67ae802d90e78b5b4bbcd2a5557
Tristan Tinning <tristan_tinning@hotmail.com>
2021-07-20 16:07:33 +0100

    book has title test pass

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before() {
+        book = new Book ("About A Boy", "Nick Hornby", "Comedy");
+    }
+
+    @Test
+    public void bookHasTitle() {
+        assertEquals("About A Boy", book.getTitle());
+    }
+
+}
